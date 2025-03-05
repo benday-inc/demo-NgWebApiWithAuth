4b707bde57f05521399aafc98f2bec4263f04b75
Benjamin Day <benday@benday.com>
2025-03-05 16:26:28 -0500

    Added test to check for duplicate username create

--- Benday.DemoApp.IntegrationTests/AuthControllerFixture.cs
+++ Benday.DemoApp.IntegrationTests/AuthControllerFixture.cs
@@ -48,4 +48,50 @@ public class AuthControllerFixture : TestClassBase
 
         Assert.Contains("User registered successfully!", responseContent);
     }
+
+    [Fact]
+    public async Task Register_Post_DuplicateUser_Fails()
+    {
+        // arrange
+        using var factory = new WebApplicationFactory<MarkerClassForTesting>();
+
+        var client = factory.CreateClient();
+
+        var now = DateTime.Now.Ticks.ToString();
+
+        var username = $"email_{now}@email.com";
+
+        var model = new RegisterModel()
+        {
+            Email = username,
+            Password = PASSWORD
+        };
+
+        var postContent = new StringContent(
+            JsonSerializer.Serialize(model), Encoding.UTF8, "application/json");
+
+        // create the user
+        await client.PostAsync("/api/auth/register", postContent);
+
+        // act
+        // attempt to create the user again
+        var response = await client.PostAsync("/api/auth/register", postContent);
+
+        // assert
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        WriteLine(responseContent);
+
+        Assert.False(response.IsSuccessStatusCode);
+
+        var errors = JsonSerializer.Deserialize<List<AuthUsernameOperationFailure>>(
+            responseContent);
+
+        Assert.NotNull(errors);
+
+        Assert.Equal(2, errors.Count);
+
+        Assert.Contains(errors, x => x.Code == "DuplicateUserName");
+        Assert.Contains(errors, x => x.Code == "DuplicateEmail");
+    }
 }
