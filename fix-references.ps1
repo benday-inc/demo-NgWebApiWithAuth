0ec431f433460a68e19c1ee2c75d437e73f56284
benday <benday@benday.com>
2025-11-12 11:29:36 -0500

    adding fix references ps1

---
+++ fix-references.ps1
@@ -0,0 +1,6 @@
+slnutil wildcardreference /filter:microsoft
+slnutil wildcardreference /filter:coverlet
+slnutil wildcardreference /filter:xunit
+slnutil wildcardreference /filter:benday
+
+slnutil cleanreferences
